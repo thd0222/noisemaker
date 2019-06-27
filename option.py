8f23e6e3ad4d750524a2c9cc4087ae70c0bbd62b
thd0222 <thdtjddb@naver.com>
2019-06-27 17:06:59 +0900

    Update option.py

--- option.py
+++ option.py
@@ -3,12 +3,12 @@ import argparse
 parser = argparse.ArgumentParser(description='Noise Maker')
 
 parser.add_argument('--src', type=str,
-                    help='name if single else path of source images.')
+                    help='name or path of source images.')
 parser.add_argument('--dst', type=str,
                     help='path of destination.')
 
 parser.add_argument('--method', type=str,
-                    help='method of noising. salt_and_pepper, gaussian.')
+                    help='method of noising. {salt_and_pepper, gaussian}')
 parser.add_argument('--p', type=float, default=0.05,
                     help='probability of salt and pepper noise.')
 parser.add_argument('--var', type=float, default=10/255.,
