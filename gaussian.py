9d1d2dcb9d27ac1b55913617b07aefc9490c01ea
thd0222 <thdtjddb@naver.com>
2019-06-27 15:20:40 +0900

    Rename sigma to var

--- gaussian.py
+++ gaussian.py
@@ -2,6 +2,6 @@ import numpy as np
 import cv2
 
 
-def generate_gaussian_noise(image, sigma):
-    normal = np.random.normal(0, sigma, image.shape)
+def generate_gaussian_noise(image, var):
+    normal = np.random.normal(0, var, image.shape)
     return image + normal
