2de500610720d79ae599fd6e159957d7a5892502
thd0222 <thdtjddb@naver.com>
2019-06-27 17:00:20 +0900

    Fix typo

--- option.py
+++ option.py
@@ -3,15 +3,15 @@ import argparse
 parser = argparse.ArgumentParser(description='Noise Maker')
 
 parser.add_argument('--src', type=str,
-                    help='name if single else path of source images')
+                    help='name if single else path of source images.')
 parser.add_argument('--dst', type=str,
-                    help='path of destination')
+                    help='path of destination.')
 
 parser.add_argument('--method', type=str,
-                    help='method of noising. salt_and_pepper, gaussian')
+                    help='method of noising. salt_and_pepper, gaussian.')
 parser.add_argument('--p', type=float, default=0.05,
-                    help='Probability of salt and pepper noise.')
+                    help='probability of salt and pepper noise.')
 parser.add_argument('--var', type=float, default=10/255.,
-                    help='Variance of gaussian noise.')
+                    help='variance of gaussian noise.')
 
 args = parser.parse_args()
